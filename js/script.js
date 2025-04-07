a5c5fb59adee2b24135c2d01a3c1aa97e209a658
anilraj-rgb <anil.raj9727@gmail.com>
2025-04-07 13:12:26 +0200

    [UPD] tarminate js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-const apiKey = ' https://lanciweb.github.io/demo/api/pictures/';
+const apiKey = " https://lanciweb.github.io/demo/api/pictures/";
 
-axios.get(apiKey)
-    .then(response => {
-        console.log(response.data)
-        //codice da eseguire in caso di successo
+axios
+  .get(apiKey)
+  .then((response) => {
+    console.log(response.data);
+    //codice da eseguire in caso di successo
 
-        response.forEach(element => {
+    response.data.forEach((element) => {
+      const cardContainer = document.querySelector(".container .row");
 
-            const cardHtmal = document.querySelectorAll('.container .row')
-            
-            cardHtmal.innerHtmal +=
-            
-          `  <div class="col-sm-12 col-md-6 col-lg-4">
+        
+        //! first version animated
+        // si Usa innerHTML per inserire tutta la struttura img
 
-                <div class="card m-3" style="width: 100%;">
+        cardContainer.innerHTML += `  
+        <div class="col-sm-12 col-md-6 col-lg-4 my-3">
 
-                    <figure class="position-relative">
-                        <img class="position-absolute top-0 start-50 translate-middle" src="./img/pin.svg" alt="pin">
-                    </figure>
+          <div class="card rotate-zoom position-relative" style="width: 100%;">
 
-                    <a href="">
-                        <img src= "${element.url}" class="card-img-top p-3" alt="...">
-                    </a>
+            <figure class=" rotation-none position-absolute top-0 start-50 translate-middle">
+              <img class="position-absolute top-0 start-50 translate-middle " src="./img/pin.svg" alt="pin">
+            </figure>
+
+            <a href="#" class="rounded" id="${element.id}"  >
+              <img  src= "${element.url}" class="card-img-top p-3 img-fluid" alt=${element.title}">
+            </a>
                         
-                    <div class="card-body">
-                      <h5 class="card-title">${element.title}</h5>
-                      <p class="card-text">${element.date}</p>
-                    </div>
-
-                </div>
-
+            <div class="card-body">
+              <p class="card-text">${element.date}</p>
+              <h5 class="card-title fw-bold text-uppercase">${element.title}</h5>
             </div>
-            `
-            
 
+          </div>
 
-            
-        });
+        </div>
+      `;
+    }
+    );
